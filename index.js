378c992d8d702442f3b9e5f48b3b9e2901452e37
Carl <carljeanpierre@gmail.com>
2024-01-17 22:05:09 -0500

    lab completed

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction(){
+    return function myNamedFunction(){
+    }
+    }
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    };
+}
